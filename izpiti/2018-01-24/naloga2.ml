2d7d684c9fa7fd76bd093082f0defab0084d738f
Nik Globocnik <nik@Niks-MacBook-Pro.local>
2020-01-26 00:24:37 +0100

    nalogi 2 in 3

--- izpiti/2018-01-24/naloga2.ml
+++ izpiti/2018-01-24/naloga2.ml
@@ -13,7 +13,22 @@ let rec vstavi x fdrevo =
 
 
 let vstavi_seznam lst fdrevo =
-  List.fold_right vstavi lst fdrevo 
+  List.fold_right vstavi lst fdrevo
+  
+let rec pravilno_vstavljen fdrevo = 
+  let preveri sp zg x =
+    match (sp, zg) with
+    | (None, None) -> true
+    | (Some l, None) -> l <= x
+    | (None, Some r) -> x < r
+    | (Some l, Some r) -> l <= x && x < r
+  in 
+  let rec vrednosti_med  sp zg fdrevo =
+    match fdrevo with 
+    | Box (bs) -> List.for_all (preveri sp zg) bs
+    | Node(v, lt, rt) -> (vrednosti_med sp (Some v) lt) && (vrednosti_med (Some v) zg rt)
+  in
+  vrednosti_med None None fdrevo
 
-
+  let primer1 = Node (10, Node(5, (Box [1]), (Box [])), Node(15, (Box [19]), (Box [19; 20])))
 
--- izpiti/2018-01-24/naloga3.ml
+++ izpiti/2018-01-24/naloga3.ml
@@ -16,3 +16,27 @@ module type Linearna = sig
   (* Vrne kompozitum danih preslikav. *)
   val kompozitum : t -> t -> t
 end
+
+module Matrika : Linearna = struct
+  type t = int*int*int*int
+
+  let id = (1, 0, 0, 1)
+  let uporabi (a, b, c, d) (x, y)  = (x*a + y*b, x*c + y*d)
+  let iz_matrike m = m
+  let iz_funkcije f =
+    let (a, c) = f (1, 0) in
+    let (b, d) = f (0, 1) in
+    (a, b, c, d)
+  let kompozitum (a, b, c, d) (e, f, g, h) =
+    (a*e + b*g, a*f + b*h, c*e + d*g, c*f + c*h)
+end
+
+module Funkcija : Linearna = struct
+  type t = int*int -> int*int
+
+  let id = (fun x -> x)
+  let uporabi f v = f v
+  let iz_matrike (a, b, c, d) = fun (x, y) -> (x*a + y*b, x*c + y*d)
+  let iz_funkcije f = f
+  let kompozitum  f g = fun x -> f(g(x))
+end
